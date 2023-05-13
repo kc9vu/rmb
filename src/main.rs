21cd7ddd41779a792247125375cda62d1c5c77e8
kc9vu <45463242+kc9vu@users.noreply.github.com>
2023-05-13 15:58:29 +0800

    push to Github

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+
+    let offset: u64 = args[1].parse().expect("偏移量非法！");
+    let mut input = File::open(&args[2]).expect("无法打开输入文件！");
+    let mut output = File::create(&args[3]).expect("无法创建输出文件！");
+
+    input.seek(SeekFrom::Start(offset)).expect("无法设置偏移量！");
+
+    let mut buffer = [0;1024];
+    loop {
+        let bytes_read = input.read(&mut buffer).expect("文件读取失败！");
+        if bytes_read == 0 { break; }
+        output.write_all(&buffer[..bytes_read]).expect("写入文件失败！");
+    }
+}
